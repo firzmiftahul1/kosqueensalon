826b2118fcd1ead63567d0c569b0a7245f9f9c3b
litanurhasanah <litanhsn@gmail.com>
2026-06-15 15:37:56 +0700

    menyelesaikan modul ass 3

---
+++ app/Filament/Resources/JournalResource/Pages/CreateJournal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\JournalResource\Pages;
+
+use App\Filament\Resources\JournalResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateJournal extends CreateRecord
+{
+    protected static string $resource = JournalResource::class;
+}
